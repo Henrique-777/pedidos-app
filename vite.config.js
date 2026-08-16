c95486dda56d46c64934f2d6cffea0a67cd52e29
Henrique-777 <pedroalvescruz2000@gmail.com>
2026-08-16 19:00:11 -0300

    fix: corrigindo import de plugin react no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs.plugin-react'
+import react from '@vitejs/plugin-react' // Use a barra "/" e não o ponto "."
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
 })
